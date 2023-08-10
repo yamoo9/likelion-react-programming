a6426a81b92bc511ffba5ae7ca096b56dd0ed206
lagom60 <lagom6061@gmail.com>
2023-08-10 15:02:53 +0900

    .

---
+++ src/layout/RootLayout.jsx
@@ -0,0 +1,14 @@
+import FooterBar from "./FooterBar";
+import HeaderBar from "./HeaderBar";
+
+function RootLayout(props) {
+  return (
+    <>
+      <HeaderBar />
+      <main>{props.children}</main>
+      <FooterBar />
+    </>
+  );
+}
+
+export default RootLayout;
