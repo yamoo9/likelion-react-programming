22f4b61e93abae0016166f7bf6c1ea701b65252f
lagom60 <lagom6061@gmail.com>
2023-08-10 11:33:39 +0900

    .

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
     devSourcemap: true,
-  }
+  },
+  resolve: {
+    // alias: [{ find: '@', replacement: path.resolve(__dirname, './src') }],
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
