70eac62901e849d2ea75933479317c1ab6bf6d7a
yamoo9 <yamoo9@naver.com>
2023-08-18 14:31:25 +0900

    React Router 시작하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
-import { useState } from 'react';
-import RootLayout from './layout/RootLayout';
-import LearnStateAndEffects from './pages/LearnStateAndEffects';
-import ProductList from './pages/ProductList';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RootLayout from "./layout/RootLayout";
+
+const Home = () => <div>Home</div>;
+const Products = () => <div>Products</div>;
+const Contact = () => <div>Contact</div>;
+
+const router = createBrowserRouter([
+  // Router Object
+  // 경로(path), 요소(element ← <Component />)
+  { path: '/', element: <Home /> },
+  { path: '/products', element: <Products /> },
+  { path: '/contact', element: <Contact /> },
+]);
+
+console.log(router);
 
 function App() {
-  const [isVisible] = useState(true);
+  
 
   return (
     <div className="App">
-      <RootLayout>
-        <LearnStateAndEffects />
-        {isVisible && <ProductList />}
-      </RootLayout>
+      <RouterProvider router={router} />
     </div>
   );
 }
