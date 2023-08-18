4bb2793e680592e812d17858ea9195c5acc9ab26
yamoo9 <yamoo9@naver.com>
2023-08-18 11:45:29 +0900

    useEffect 훅 그리고 통신 중단(AbortController)

--- src/pages/LearnStateAndEffects.jsx
+++ src/pages/LearnStateAndEffects.jsx
@@ -18,9 +18,12 @@ function LearnStateAndEffects() {
   // 이펙트가 필요해!!!
   // React 외적인 일을 처리
   useEffect(() => {
+    const controller = new AbortController();
+    const { signal } = controller;
+
     setStatus('loading');
 
-    fetch('http://127.0.0.1:8090/api/collections/todos/records')
+    fetch('http://127.0.0.1:8090/api/collections/todos/records', { signal })
       .then((response) => response.json())
       .then((responseData) => {
         setStatus('success');
@@ -30,6 +33,10 @@ function LearnStateAndEffects() {
         setStatus('error');
         setError(error);
       });
+
+    return () => {
+      controller.abort();
+    }
   }, []);
 
   // 함수 몸체: 문 또는 식, 함수
--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,5 +1,5 @@
-import { getPbImageURL, numberWithComma } from "@/utils";
-import { useEffect, useState } from "react"
+import { getPbImageURL, numberWithComma } from '@/utils';
+import { useEffect, useState } from 'react';
 
 const PB_PRODUCTS_ENDPOINT = `
   http://127.0.0.1:8090/api/collections/products/records
@@ -11,11 +11,16 @@ function ProductList() {
   const [error, setError] = useState(null); // null | Error
 
   useEffect(() => {
+    // 중단(abort) 컨트롤러(controller) 생성
+    const controller = new AbortController();
+
     setIsLoading(true);
 
     async function fetchProducts() {
       try {
-        const response = await fetch(PB_PRODUCTS_ENDPOINT)
+        const response = await fetch(PB_PRODUCTS_ENDPOINT, {
+          signal: controller.signal,
+        });
         const responseData = await response.json();
         console.log(responseData);
         setData(responseData);
@@ -27,8 +32,14 @@ function ProductList() {
     }
 
     fetchProducts();
+
+    // StrictMode(x2, detect impure function component)
+    // mount(1, 요청 1) → unmount (취소 1) → mount(2, 요청 1) -> 응답 1
+    return () => {
+      controller.abort();
+    }
   }, []);
-  
+
   // javascript statement
   // if (data) {
   //   if ('items' in data) {
@@ -43,18 +54,22 @@ function ProductList() {
   return (
     <ul className="grid grid-cols-3 m-10">
       {/* javascript expression */}
-      { data && data.items && data.items?.map(item => (
-        <ProductItem key={item.id} item={item} />
-      )) }
+      {data &&
+        data.items &&
+        data.items?.map((item) => <ProductItem key={item.id} item={item} />)}
     </ul>
-  )
+  );
 }
 
-function ProductItem({ item}) {
+function ProductItem({ item }) {
   return (
     <li>
-      <figure className="flex flex-col items-start">        
-        <img src={getPbImageURL(item, 'photo')} className="h-96 w-auto" alt="" />
+      <figure className="flex flex-col items-start">
+        <img
+          src={getPbImageURL(item, 'photo')}
+          className="h-96 w-auto"
+          alt=""
+        />
         <figcaption className="flex flex-col">
           <span className="title">
             {item.title} [ {item.color} ]
@@ -66,4 +81,4 @@ function ProductItem({ item}) {
   );
 }
 
-export default ProductList
+export default ProductList;
