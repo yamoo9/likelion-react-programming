849d2065361e64e4c23c3611d9f99ee2f750b5f0
yamoo9 <yamoo9@naver.com>
2023-08-23 11:07:27 +0900

    Props 전달과 성능 이슈 (디바운싱)

--- src/learn/1-passing-props.jsx
+++ src/learn/1-passing-props.jsx
@@ -1,3 +1,4 @@
+import debounce from '@/utils/debounce';
 import { useState } from 'react';
 
 function PassingProps() {
@@ -13,10 +14,7 @@ export default PassingProps;
 /* -------------------------------------------------------------------------- */
 
 export function Parent() {
-  // 상태
   const [color, setColor] = useState('#999');
-
-  // 상태 업데이트 이벤트 핸들러
   const handleChangeBgColor = (newColor) => setColor(newColor);
 
   return (
@@ -29,11 +27,14 @@ export function Parent() {
 export function Child({ color, onChangeColor }) {
   return (
     <div className="Child flex items-center justify-center gap-8">
-      <p className='text-4xl font-extralight uppercase' style={{ color }}>Child</p>
+      <p className="text-4xl font-extralight uppercase" style={{ color }}>
+        Child
+      </p>
       <input
         type="color"
         aria-label="글자 색상 변경"
-        onChange={(e) => onChangeColor(e.target.value)}
+        defaultValue={color}
+        onChange={debounce((e) => onChangeColor(e.target.value), 500)}
       />
     </div>
   );
