de9fc3bf5a5c9ad9f0b84677a5ac460d4d88d685
yamoo9 <yamoo9@naver.com>
2023-08-23 10:57:53 +0900

    속성(Props) 전달 & Props 드릴링 이슈

--- src/learn/1-passing-props.jsx
+++ src/learn/1-passing-props.jsx
@@ -1,34 +1,40 @@
+import { useState } from 'react';
+
 function PassingProps() {
-  return <div className="PassingProps">
-    <GrandParent>
-      <Parent>
-        <Child>
-          <GrandChild>
-            <p>데이터를 주세요.</p>
-          </GrandChild>
-        </Child>
-      </Parent>
-    </GrandParent>
-  </div>;
+  return (
+    <div className="PassingProps">
+      <Parent />
+    </div>
+  );
 }
 
 export default PassingProps;
 
 /* -------------------------------------------------------------------------- */
 
-export function GrandParent({ children }) {
-  return <div className="GrandParent">{children}</div>;
+export function Parent() {
+  // 상태
+  const [color, setColor] = useState('#999');
+
+  // 상태 업데이트 이벤트 핸들러
+  const handleChangeBgColor = (newColor) => setColor(newColor);
+
+  return (
+    <div className="Parent">
+      <Child color={color} onChangeColor={handleChangeBgColor} />
+    </div>
+  );
 }
 
-export function Parent({ children }) {
-  return <div className="Parent">{children}</div>;
+export function Child({ color, onChangeColor }) {
+  return (
+    <div className="Child flex items-center justify-center gap-8">
+      <p className='text-4xl font-extralight uppercase' style={{ color }}>Child</p>
+      <input
+        type="color"
+        aria-label="글자 색상 변경"
+        onChange={(e) => onChangeColor(e.target.value)}
+      />
+    </div>
+  );
 }
-
-export function Child({ children }) {
-  return <div className="Child">{children}</div>;
-}
-
-export function GrandChild({ children }) {
-  return <div className="GrandChild">{children}</div>;
-}
-
--- src/learn/3-props-drilling-issue.jsx
+++ src/learn/3-props-drilling-issue.jsx
@@ -1,5 +1,64 @@
+import { useState } from 'react';
+
 function PropsDrillingIssue() {
-  return <></>;
+  // 상태
+  const [color, setColor] = useState({
+    fg: 'text-rose-600',
+    bg: '#93c5fe',
+  });
+  // 상태 업데이트 이벤트 핸들러
+  const handleChangeBgColor = (newBgColor) =>
+    setColor((color) => ({
+      ...color,
+      bg: newBgColor,
+    }));
+
+  return (
+    <div className="PassingProps" style={{ backgroundColor: color.bg }}>
+      <GrandParent color={color} onChangeColor={handleChangeBgColor} />
+    </div>
+  );
+}
+
+/* -------------------------------------------------------------------------- */
+
+export function GrandParent({ color, onChangeColor }) {
+  return (
+    <div className="GrandParent">
+      <Parent color={color} onChangeColor={onChangeColor} />
+    </div>
+  );
+}
+
+export function Parent({ color, onChangeColor }) {
+  return (
+    <div className="Parent">
+      <Child color={color} onChangeColor={onChangeColor} />
+    </div>
+  );
+}
+
+export function Child({ color, onChangeColor }) {
+  return (
+    <div className="Child">
+      <GrandChild color={color} onChangeColor={onChangeColor} />
+    </div>
+  );
+}
+
+export function GrandChild({ color, onChangeColor }) {
+  return (
+    <div className="GrandChild">
+      <p className={color.fg}>상태 데이터를 제게 주세요!</p>
+      <input
+        type="color"
+        aria-label="배경 색상"
+        onChange={(e) => {
+          onChangeColor(e.target.value);
+        }}
+      />
+    </div>
+  );
 }
 
 export default PropsDrillingIssue;
