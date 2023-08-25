b80815c69e02e630940f107d2acc1364a81e183a
yamoo9 <yamoo9@naver.com>
2023-08-25 13:13:38 +0900

    인증에 따른 라우팅 접근 시도 중 발생한 경고 해결
    - 렌더링에 문제를 일으키는 상태 변경 요청 코드를 useEffect 내부로 옮김 (사이드 이펙트)

--- src/components/ProtectRoute.jsx
+++ src/components/ProtectRoute.jsx
@@ -1,21 +1,26 @@
 import { useAuth } from '@/contexts/Auth';
 import { element } from 'prop-types';
+import { useEffect } from 'react';
 import toast from 'react-hot-toast';
 import { Navigate } from 'react-router-dom';
 
 function ProtectRoute({ children }) {
   const { isAuth } = useAuth();
 
+  useEffect(() => {
+    if (!isAuth) {
+      toast('로그인 된 사용자만 이용 가능한 페이지입니다.', {
+        position: 'top-right',
+        icon: '🚨',
+        ariaProps: {
+          role: 'alert',
+          'aria-live': 'polite',
+        },
+      });
+    }
+  }, [isAuth]);
+  
   if (!isAuth) {
-    toast('로그인 된 사용자만 이용 가능한 페이지입니다.', {
-      position: 'top-right',
-      icon: '🚨',
-      ariaProps: {
-        role: 'alert',
-        'aria-live': 'polite',
-      },
-    });
-
     return <Navigate to="/signin" />;
   }
 
