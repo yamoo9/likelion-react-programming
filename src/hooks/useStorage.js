a2023e68afc27bdabe7799c97b606019a38153ab
yamoo9 <yamoo9@naver.com>
2023-08-25 10:51:48 +0900

    useCallback vs. useMemo
    - useCallback : 함수 값만 기억
    - useMemo : JS 모든 값(함수 포함)을 기억

--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -3,22 +3,22 @@
 // 2. key 값으로 로컬(또는 세션) 스토리지에서 데이터 읽기(JavaScript 데이터 타입 변환된 값)
 // 3. key 값으로 로컬(또는 세션) 스토리지 데이터 삭제
 
-import { useLayoutEffect, useState } from 'react';
+import { useCallback, useLayoutEffect, useMemo, useState } from 'react';
 
 const {
   localStorage: storage,
   JSON: { stringify: serialize, parse: deserialize },
 } = globalThis;
 
-export const setData = (key, nextData) => {
+const setData = (key, nextData) => {
   storage.setItem(key, serialize(nextData));
 };
 
-export const getData = (key) => {
+const getData = (key) => {
   return deserialize(storage.getItem(key));
 };
 
-export const deleteData = (key) => {
+const deleteData = (key) => {
   storage.removeItem(key);
 };
 
@@ -30,8 +30,42 @@ function useStorage(key) {
     setStorageData(data);
   }, [key]);
 
+  // useCallback : 함수 값만 기억 vs. useMemo : JS 모든 값(함수 포함)을 기억
+  // useMemo 훅: 모든 JS 값 유형 기억
+  // const cachedUpdate = useMemo(() => 함수 값, [])
+
+  // useCallback 훅: JS 함수 값만 기억
+
+  const update = useCallback(
+    (nextData) => {
+      setData(key, nextData);
+      setStorageData(nextData);
+    },
+    [key]
+  );
+
+  // const updateMemo = useMemo(
+  //   () => (nextData) => {
+  //     setData(key, nextData);
+  //     setStorageData(nextData);
+  //   },
+  //   [key]
+  // );
+
+  const remove = useCallback(
+    () => { deleteData(key); }, 
+    [key]
+  );
+
+  // const removeMemo = useMemo(
+  //   () => () => { deleteData(key); },
+  //   [key]
+  // );
+
   return {
     storageData,
+    update,
+    remove,
   };
 }
 
--- src/learn/10-local-storage.jsx
+++ src/learn/10-local-storage.jsx
@@ -1,7 +1,36 @@
+import useStorage from '@/hooks/useStorage';
+
 function LocalStorage() {
+  const { storageData, update, remove } = useStorage('yamoo9');
+  const { update2, remove2 } = useStorage('mama');
+
   return (
-    <div>LocalStorage</div>
-  )
+    <>
+      <h2>로컬 스토리지 관리</h2>
+
+      <button
+        onClick={() => {
+          update()
+        }}
+        type="button"
+        className="border border-slate-300 p-2 my-2"
+      >
+        저장
+      </button>
+
+      <div className="flex flex-col gap-2">
+        <label htmlFor="storage-data">스토리지 데이터</label>
+        <input
+          type="text"
+          id="storage-data"
+          placeholder="기억할 메시지 작성"
+          className="border border-slate-300"
+        />
+
+        <p>{storageData ? '데이터가 존재' : '데이터 없음'}</p>
+      </div>
+    </>
+  );
 }
 
-export default LocalStorage
+export default LocalStorage;
