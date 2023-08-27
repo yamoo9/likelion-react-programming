e59d90ef644c06b33ab889072826ff123981c865
yamoo9 <yamoo9@naver.com>
2023-08-27 17:09:56 +0900

    로고 애니메이션 적용

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,12 +1,14 @@
 import { Link } from 'react-router-dom';
-import Logo from './Logo';
+import { FramerLogo } from './Logo';
 
 function Heading() {
-  return <h1>
-    <Link to="/">
-      <Logo size={40} className="text-blue-300" />
-    </Link>
-  </h1>;
+  return (
+    <h1>
+      <Link to="/">
+        <FramerLogo size={70} className="text-blue-300" />
+      </Link>
+    </h1>
+  );
 }
 
 export default Heading;
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,6 +1,7 @@
 import { number } from 'prop-types';
+import { motion } from 'framer-motion';
 
-function Logo({ size = 100, ...restProps }) {
+export function Logo({ size = 100, ...restProps }) {
   return (
     <svg
       width={size}
@@ -23,4 +24,67 @@ Logo.propTypes = {
   size: number,
 };
 
-export default Logo;
+/* -------------------------------------------------------------------------- */
+
+const createIconVariants = (color) => ({
+  hidden: {
+    pathLength: 0,
+    fill: `${color}00`,
+  },
+  visible: {
+    pathLength: 1,
+    fill: `${color}FF`,
+  },
+});
+
+export function FramerLogo({ size = 60 }) {
+  return (
+    <svg width={size} height={size} viewBox="0 0 32 32" fill="none">
+      <motion.path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M9 20.3335H16V27.0002L9 20.3335Z"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        className="stroke-blue-400 stroke-[0.3px]"
+        variants={createIconVariants('#0055FF')}
+        initial="hidden"
+        animate="visible"
+        transition={{
+          default: { duration: 0.75, ease: 'easeInOut' },
+          fill: { duration: 1, ease: [1, 0, 0.8, 1] },
+        }}
+      />
+      <motion.path
+        d="M16 13.6665H9V20.3332H23L16 13.6665Z"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        className="stroke-blue-400 stroke-[0.3px]"
+        variants={createIconVariants('#00AAFF')}
+        initial="hidden"
+        animate="visible"
+        transition={{
+          default: { duration: 0.75, delay: 0.25, ease: 'easeInOut' },
+          fill: { duration: 1, delay: 0.25, ease: [1, 0, 0.8, 1] },
+        }}
+      />
+      <motion.path
+        d="M9 7L16 13.6667H23V7H9Z"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        className="stroke-blue-400 stroke-[0.3px]"
+        variants={createIconVariants('#88DDFF')}
+        initial="hidden"
+        animate="visible"
+        transition={{
+          default: { duration: 0.75, delay: 0.5, ease: 'easeInOut' },
+          fill: { duration: 1, delay: 0.5, ease: [1, 0, 0.8, 1] },
+        }}
+      />
+    </svg>
+  );
+}
+
+FramerLogo.propTypes = {
+  size: number,
+};
